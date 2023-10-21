2249964e2eead97f3e4f4da3aa654378a5eb4809
Kevin Carr <kcarr030@odu.edu>
2023-10-21 11:44:19 -0400

    Added Section Database - KEVIN

--- catalogue/models.py
+++ catalogue/models.py
@@ -7,3 +7,16 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+class Section(models.Model):
+    course = models.ForeignKey(Course, blank=True, null=True, on_delete=models.CASCADE)
+    courseID = models.IntegerField()
+    semester = models.CharField('Semester', max_length=50)
+    session = models.CharField('Session', max_length=25)
+    offeringTime = models.TimeField(auto_now=False, auto_now_add=False)
+    professor = models.CharField('Professor Name', max_length=120)
+    deliveryType = models.CharField(max_length=25)
+    meetingType = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
