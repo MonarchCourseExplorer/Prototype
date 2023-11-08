196be9d1faf621c232f58ef49057ce51828f5fdb
Phillip <phillipewj@gmail.com>
2023-11-07 19:36:22 -0500

    Added Functionality: Faculty Login

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,10 @@ from .import views
 
 urlpatterns = [
     path('test', views.test, name="test"),
+    path('test2', views.test_two, name="test2"),
+    path('test3', views.test_three, name="test3"),
     path('login_user/', views.login_user, name = "login"),
+    path('login_faculty_user/', views.login_faculty_user, name = "flogin"),
     path('logout_user/', views.logout_user, name = "logout"),
     path('register', views.register_user, name= "register" ),
     
--- users/views.py
+++ users/views.py
@@ -7,6 +7,12 @@ from .forms import SignUpForm
 def test(request):
     return render(request,'test.html')
 
+def test_two(request):
+    return render(request,'authenticate/test2.html')
+
+def test_three(request):
+    return render(request,'authenticate/test3.html')
+
 def login_user(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -14,7 +20,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('test')
+            return redirect('test3')
             # Redirect to a success page.
         else:
             # Return an 'invalid login' error message.
@@ -24,8 +30,23 @@ def login_user(request):
     else:
         return render(request, 'authenticate/login.html', {})
 
-#def home(request):
-    #return render(request, 'pages/gallery.html')
+def login_faculty_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('test2')
+            # Redirect to a success page.
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ("There Was An Error Logging In, Please Try Again"))
+            return redirect('flogin')
+
+    else:
+        return render(request, 'authenticate/faculty_login.html', {})
+
 def logout_user(request):
     logout(request)
     return redirect('test')
