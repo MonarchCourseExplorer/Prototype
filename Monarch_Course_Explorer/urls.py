5fc5f8cdf7dc0f478344638731e2bde370dab825
WilliamVarnier <wvarn001@odu.edu>
2023-11-09 01:00:19 -0500

    Updated URLs and views
    Removed unused URLs and updated the home page's link to the provide feedback page

--- Monarch_Course_Explorer/urls.py
+++ Monarch_Course_Explorer/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from Monarch_Course_Explorer import views
 
 urlpatterns = [
@@ -23,13 +23,6 @@ urlpatterns = [
     path('index.html', views.homeView, name='home'),
     path('admin/', admin.site.urls),
     path('pages/gallery.html', views.galleryView, name='gallery'),
-    path('pages/portfolio.html', views.portfolioView, name='portfolio'),
-    path('pages/full-width.html', views.fullWidthView, name='full-width'),
-    path('pages/sidebar-left.html', views.sidebarLeftView, name='sidebar-left'),
-    path('pages/sidebar-left-2.html', views.sidebarLeftView2, name='sidebar-left 2'),
-    path('pages/sidebar-right.html', views.sidebarRightView, name='sidebar-right'),
-    path('pages/sidebar-right-2.html', views.sidebarRightView2, name='sidebar-right 2'),
-    path('pages/basic-grid.html', views.basicGridView, name='basic-grid'),
     path('pages/provideFeedback.html', views.provideFeedbackView, name='provideFeedback'),
     path('users/', include('django.contrib.auth.urls')) ,
     path('users/', include('users.urls')) 
