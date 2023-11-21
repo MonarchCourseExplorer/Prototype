44dac2fb2563984ec8938b250f3c752f92d3c42f
kevin <kcarr030@odu.edu>
2023-11-21 16:22:25 -0500

    comment out line of code

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quiz/<int:question_id>/', views.quiz_question, name='quiz_question'),
+    #path('quiz/<int:question_id>/', views.quiz_question, name='quiz_question'),
     #path('submit_answer/', views.submit_answer, name='submit_answer'),
     #path('recommendation/', views.show_recommendation, name='show_recommendation'),
     path('upload/', views.upload_Syllabus, name= 'upload'),
