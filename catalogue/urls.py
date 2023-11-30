f6ef8e3825cf9d06dbebd1e3629e9e60c2dc8f08
Phillip Wilson <99518654+Philewj@users.noreply.github.com>
2023-11-29 21:31:51 -0500

    Update urls.py

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #path('submit_answer/', views.submit_answer, name='submit_answer'),
     #path('recommendation/', views.show_recommendation, name='show_recommendation'),
     #path('pages/provideFeedback.html', views.provideFeedback, name='provideFeedback'),
-    path('add_feedback', views.add_feedback, name='add-venu'),
+    #path('add_feedback', views.add_feedback, name='add-venu'),
     path('upload/', views.upload_Syllabus, name= 'upload'),
     path('success', views.success, name="success"),
 
