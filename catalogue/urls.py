f344cc4a960b7c10aa65500d6287143ea08b160b
Kelsey Marantan <kelsey.marantan@gmail.com>
2023-12-05 00:02:23 -0500

    placeholder syllabus upload page

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('upload/', views.uploadSyllabus, name= 'upload'),
     path('success', views.success, name="success"),
+    path('syllabusSuccess', views.syllabusSuccess, name="syllabusSuccess"),
     path('provide_feedback/', views.provideFeedback, name='provide_feedback'),
     
 
